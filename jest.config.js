9021449f02485b8c60bb933c06f51e4216f1bcac
Alejandro Cerezo <alce65@hotmail.es>
2023-05-23 17:53:38 +0200

    🔧 Add and configure Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["dist"],
+  resolver: "jest-ts-webcompat-resolver",
+};
